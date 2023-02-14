33837de7e5bd31f90ad3ca63af5c86dbf33e52ab
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2023-02-14 17:04:21 +0500

    Created new Project

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+
+import { Inter } from '@next/font/google'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+     <h2>Hello NextJS</h2>
+    </>
+  )
+}
