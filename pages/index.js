faad64c2cc91bfd851ed8e5cb70ee0770c89636b
Ghulam Muhammad <ghulamhafeez.dev@gmail.com>
2023-02-16 18:28:20 +0500

    Completed nextJs Photos

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,37 @@
+import Link from "next/link";
+import Image from "next/image";
+export default function Home({ data }) {
+  const myLoader = ({ src, width, quality }) => {
+    return `${src}?w=${width}&q=${quality || 75}`;
+  };
 
-import { Inter } from '@next/font/google'
-
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
   return (
-    <>
-     <h2>Hello NextJS</h2>
-    </>
-  )
+    <div>
+      <h2>NextJs Photos</h2>
+      {data?.slice(0, 20).map((x) => {
+        return (
+          <Link href={`/photos/${x.id}`} key={x.id}>
+            <Image
+              loader={myLoader}
+              src={x.url}
+              alt="Picture"
+              width={150}
+              height={150}
+            />
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+  const data = await res.json();
+
+  return {
+    props: {
+      data: data,
+    },
+  };
+};
